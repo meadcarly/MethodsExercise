9859314f68c64d53f45d2452272f33aa7351dafc
Carly Mead <meadcarly16@gmail.com>
2024-07-09 12:25:55 -0600

    Created a method to get user input and tell a story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,29 @@
 {
     public class Program
     {
+        public static void StoryInput()
+        {
+        Console.WriteLine("What is your name?)");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("What is your favorite color?");
+        string color = Console.ReadLine();
+
+        Console.WriteLine("What is your favorite animal?");
+        string animal = Console.ReadLine();
+
+        Console.WriteLine("What is your favorite band?");
+        string band = Console.ReadLine();
+        
+        Console.WriteLine($"Once upon a time {name} was walking through the art museum listening to {band}. {name} stopped at a {color} painting and was staring at it for some time before noticing that a {animal} was blended into the background. She jumped when she saw the {animal} and, as she clutched her chest, she thought to herself {name}, if it has been a snake it would have bitten you");
+        }   
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+         StoryInput();
         }
     }
 }
+
+
+
