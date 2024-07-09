7ba8f603e8e3d33d2f80aaeb44f135554027ee0d
Carly Mead <meadcarly16@gmail.com>
2024-07-09 13:29:01 -0600

    I added the math methods for addition, subtraction, multiplication, and division, then I added the params keyword and a foreach loop and corrected the initial value of each of the math problems.

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -16,12 +16,63 @@
         Console.WriteLine("What is your favorite band?");
         string band = Console.ReadLine();
         
-        Console.WriteLine($"Once upon a time {name} was walking through the art museum listening to {band}. {name} stopped at a {color} painting and was staring at it for some time before noticing that a {animal} was blended into the background. She jumped when she saw the {animal} and, as she clutched her chest, she thought to herself {name}, if it has been a snake it would have bitten you");
-        }   
+        Console.WriteLine($"Once upon a time {name} was walking through the art museum listening to {band}. {name} stopped at a {color} painting and was staring at it for some time before noticing that a {animal} was blended into the background. {name} jumped when she saw the {animal} and, as she clutched her chest, she thought to herself {name}, if it has been a snake it would have bitten you!");
+        }
+
+        public static int CalculatesSum(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
         
+        public static int SubInt(params int[] numbers)
+        {
+            int subtract = numbers[0];
+            foreach (int number in numbers.Skip(1))
+            {
+                subtract -= number;
+            }
+            return subtract;
+        }
+        
+        public static int Times(params int[] numbers)
+        {
+            int multiply = numbers[0];
+            foreach (int number in numbers.Skip(1))
+            {
+                multiply *= number;
+            }
+            return multiply;
+        }
+        
+        public static int Division(params int[] numbers)
+        {
+            int divide = numbers[0];
+            foreach (int number in numbers.Skip(1))
+            {
+                divide /= number;
+            }
+            return divide;
+        }
         static void Main(string[] args)
         {
          StoryInput();
+         
+         int sum = CalculatesSum(5, 4, 6);
+         Console.WriteLine(sum);
+         
+         int subtract = SubInt(5, 4, 1);
+         Console.WriteLine(subtract);
+
+         int multiply = Times(5, 4, 5);
+         Console.WriteLine(multiply);
+
+         int divide = Division(20, 4, 1);
+         Console.WriteLine(divide);
         }
     }
 }
